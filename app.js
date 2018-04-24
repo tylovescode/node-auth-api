827cd39496ac1c7bb45cce8b986cd28043507a3d
Ty Miller <ty.loves.code@gmail.com>
2018-04-24 19:11:37 -0400

    added login route

--- app.js
+++ app.js
@@ -15,4 +15,20 @@ app.post('/api/posts', (req, res) => {
 	});
 });
 
+app.post('/api/login', (req, res) => {
+	//Mock user
+	const user = {
+		id: 1,
+		username: 'ty',
+		email: 'ty@tymiller.net'
+	}
+
+	//Payload, Secret Key, Callback
+	jwt.sign({user}, 'secretkey', (err, token) => {
+		res.json({
+			token
+		});
+	});
+});
+
 app.listen(5000, () => console.log('Server started on port 5000'));
